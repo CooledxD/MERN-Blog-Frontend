bfafbfbcd09367d12831039ce2d852281a342851
CooledxD <scribe_85@mail.ru>
2023-02-06 15:21:24 +0700

    added redirect in the absence of authentication

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
-import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom'
-import { useDispatch } from 'react-redux';
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route, Navigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux';
 
 import { Home } from './pages/home/home.js'
 import { Post } from './pages/post/post.js'
@@ -18,23 +18,23 @@ import { getMe } from './redux/features/auth/authSlice.js';
 
 import './index.css'
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<Layout />} errorElement={<ErrorPage />} >
-      <Route index element={<Home />} />
-      <Route path='posts' element={<Posts />} />
-      <Route path='post/add' element={<AddPost />} />
-      <Route path='post/:id' element={<Post />} />
-      <Route path='post/:id/edit' element={<EditPost />} />
-      <Route path='register' element={<Register />} />
-      <Route path='login' element={<Login />} />
-      <Route path='profile' element={<Profile />} />
-    </Route>
-  )
-)
-
 function App() {
   const dispatch = useDispatch()
+  const isAuth = useSelector((state) => Boolean(state.auth.token))
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Layout />} errorElement={<ErrorPage />} >
+        <Route index element={<Home />} />
+        <Route path='posts' element={isAuth ? <Posts /> : <Navigate to='/' />} />
+        <Route path='post/add' element={isAuth ? <AddPost /> : <Navigate to='/' />} />
+        <Route path='post/:id' element={<Post />} />
+        <Route path='post/:id/edit' element={isAuth ? <EditPost /> : <Navigate to='/' />} />
+        <Route path='register' element={<Register />} />
+        <Route path='login' element={<Login />} />
+        <Route path='profile' element={isAuth ? <Profile /> : <Navigate to='/' />} />
+      </Route>
+    )
+  )
 
   useEffect(() => {
     dispatch(getMe())
