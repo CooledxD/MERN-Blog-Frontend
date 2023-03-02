111ba85859b29f4c3e4a1ca62fe2634cdafa3f3a
CooledxD <scribe_85@mail.ru>
2023-03-02 18:16:35 +0700

    components: create private route

---
+++ src/components/privateRoute/privateRoute.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Navigate, Outlet, useLocation } from 'react-router-dom'
+
+import { renewAccessToken } from "../../redux/features/auth/authSlice.js";
+
+export const PrivateRoute = () => {
+  const isAuth = sessionStorage.getItem('isAuth')
+  const dispatch = useDispatch()
+  const location = useLocation()
+
+  useEffect(() => {
+    dispatch(renewAccessToken())
+  }, [dispatch, location])
+
+  return isAuth ? <Outlet/> : <Navigate to='/' />
+}
