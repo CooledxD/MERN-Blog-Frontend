e028f0914339c5912e9e72f247e90a4c17a1a156
CooledxD <scribe_85@mail.ru>
2023-03-10 05:22:57 +0700

    refactoring

--- src/app.js
+++ src/app.js
@@ -3,16 +3,16 @@ import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } 
 import { useDispatch } from 'react-redux';
 
 // Pages
-import { Home } from './pages/home/home.js'
-import { Post } from './pages/post/post.js'
-import { Posts } from './pages/posts/posts.js'
-import { AddPost } from './pages/addPost/addPost.js'
-import { Register } from './pages/register/register.js'
-import { Login } from './pages/login/login.js'
-import { EditPost } from './pages/editPost/editPost.js'
-import { Profile } from './pages/profile/profile.js'
-import { ErrorPage } from './pages/errorPage/errorPage.js'
-import { AccountActivation } from './pages/accountActivation/accountActivation.js';
+import { Home } from './pages/Home/Home.js'
+import { Post } from './pages/Post/Post.js'
+import { Posts } from './pages/Posts/Posts.js'
+import { AddPost } from './pages/AddPost/AddPost.js'
+import { Register } from './pages/Register/Register.js'
+import { Login } from './pages/Login/Login.js'
+import { EditPost } from './pages/EditPost/EditPost.js'
+import { Profile } from './pages/Profile/Profile.js'
+import { ErrorPage } from './pages/ErrorPage/ErrorPage.js'
+import { AccountActivation } from './pages/AccountActivation/AccountActivation.js';
 
 // Components
 import { Layout } from './components/Layout/Layout.js';
